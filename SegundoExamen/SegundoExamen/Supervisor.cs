1334e8f32c1eb84e19252afc36e72e6ca66e1fc9
José Luis Lobera del Castillo <40039535+JHludwolf@users.noreply.github.com>
2022-03-31 22:57:49 -0600

    Alumni API

---
+++ SegundoExamen/SegundoExamen/Supervisor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace SegundoExamen
+{
+	public class Supervisor: Usuario
+	{
+		public Supervisor(string id, string nombre, string apellido,
+			string carrera, string nacimiento, string ciudad, string contrasena) :
+			base(id, nombre, apellido, carrera, nacimiento, ciudad, contrasena)
+		{
+		}
+
+		public override void ConsultarInformacion()
+		{
+			Console.WriteLine(this.id + " | " + this.nombre + " " + this.apellido + "\n" + nacimiento + " | " + ciudad);
+		}
+	}
+}
+
