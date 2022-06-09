561a119885753ceeb5652c6a67e7cd0306c70a08
Hludwolf <40039535+JHludwolf@users.noreply.github.com>
2022-06-08 20:47:29 -0500

    Entrega Final

---
+++ EntregaFinal/ProyectoFinal/DeliveryTruck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal
+{
+    public class DeliveryTruck: DeliveryVehicle
+    {
+        //public string deliveryMan;
+        //public Product DeliveryProduct;
+        public DeliveryTruck(float weightCapacity): base(weightCapacity)
+        {
+            //this.DeliveryProduct = DeliveryProduct;
+        }
+    }
+}
