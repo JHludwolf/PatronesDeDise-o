ff5837d88d5acae6331523af575bc1e086d1d614
José Luis Lobera del Castillo <40039535+JHludwolf@users.noreply.github.com>
2022-06-08 20:43:33 -0500

    Add files via upload

---
+++ ProyectoFinal/EntregaFinal/Log.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal
+{
+    public class Log
+    {
+        public int userId { get; }
+        public int eventId { get; }
+        public DateTime dateTime { get; }
+
+        public Log(int userId, int EventId, DateTime dateTime) {
+            this.userId = userId;
+            this.eventId = EventId;
+            this.dateTime = dateTime;
+        }
+    }
+}
